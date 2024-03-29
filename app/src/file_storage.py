d23dc3d3adba38f2d064f7bd55d9e42aa8a5f7f2
Szabó-Galiba Máté <71736138+MicrosoftEggshell@users.noreply.github.com>
2024-03-29 23:21:16 +0100

    Implement write_events_to_file() (#2)

--- app/src/file_storage.py
+++ app/src/file_storage.py
@@ -1,4 +1,5 @@
 import json
+from typing import List
 from .models import Organizer, Joiner, Event
 
 
@@ -8,7 +9,7 @@ class EventFileManager:
     @classmethod
     def read_events_from_file(cls):
         try:
-            with open(cls.FILE_PATH) as f:
+            with open(cls.FILE_PATH, encoding='utf-8') as f:
                 content = json.load(f)
             events = []
             for event in content:
@@ -21,3 +22,14 @@ class EventFileManager:
             return events
         except (OSError, json.JSONDecodeError):
             return []
+    
+    @classmethod
+    def write_events_to_file(cls, events: List[Event]):
+        with open(cls.FILE_PATH, 'w', encoding='utf-8') as f:
+            json.dump(
+                events,
+                f,
+                ensure_ascii=False,
+                indent=4,
+                default=lambda x: x.__dict__
+                )
